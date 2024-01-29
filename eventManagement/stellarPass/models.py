c010b80ffc60d39e373801a954687b1011d11d51
moimoiixralte <sm@mms-MacBook-Pro.local>
2024-01-29 14:34:16 +0530

    made changes in models

--- eventManagement/stellarPass/models.py
+++ eventManagement/stellarPass/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Reservation(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone_number = models.IntegerField(max_length=10)
+    company_organization = models.CharField(max_length=50)
+    venue_requested = models.CharField(max_length=255)
+    type_event = models.CharField(max_length=255)
+    date_requested_first = models.DateField()
+    date_requested_second = models.DateField()
+    about_event_hosting = models.TextField()
+
+    def _str_(self):
+        return self.name
