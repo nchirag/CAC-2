cc066b5fb492d357bd1a6bfce45c7d63400236a0
Chirag N <139685474+nchirag@users.noreply.github.com>
2024-02-03 15:18:55 +0530

    sql fix

--- eventManagement/stellarPass/views.py
+++ eventManagement/stellarPass/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from .forms import ReservationForm
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login,logout
